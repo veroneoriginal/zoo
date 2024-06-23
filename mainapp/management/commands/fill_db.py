6388cf204d1941c31745a4adf874d4b6a48b8fd4
veroneoriginal <veronikanizovaya@gmail.com>
2024-06-23 15:02:41 +0300

    finish this lesson and practic

--- mainapp/management/commands/fill_db.py
+++ mainapp/management/commands/fill_db.py
@@ -14,7 +14,6 @@ class Command(BaseCommand):
         bear = Category.objects.create(name='Медведь')
         tiger = Category.objects.create(name='Тигр')
 
-
         print('Delete food ... ')
         Food.objects.all().delete()
 
@@ -22,6 +21,40 @@ class Command(BaseCommand):
         banana = Food.objects.create(name='Банан')
         meat = Food.objects.create(name='Мясо')
         honey = Food.objects.create(name='Мед')
+
+        print('Create animals ... ')
+        leo = Animal.objects.create(name="Leo", category=tiger)
+        boris = Animal.objects.create(name="Boris", category=bear)
+
+        # many to many - многие ко многим
+        leo.food.add(meat)
+        leo.food.add(banana)
+        leo.save()
+
+        boris.food.add(meat)
+        boris.food.add(honey)
+        boris.save()
+
+        print('Update ... ')
+        leo.name = "Tom"
+        leo.save()
+
+        print('Get all ... ')
+        animals = Animal.objects.all()
+        for animal in animals:
+            print(animal.name)
+            print(animal.category.name)
+            for food in animal.food.all():
+                print(food.name)
+
+        print('Get one ... ')
+        print('First')
+        first_animal = Animal.objects.filter(id=leo.id).first()
+        print('first_animal =', first_animal.name)
+
+        print('Get')
+        get_animal = Animal.objects.get(id=leo.id)
+        print('first_animal =', get_animal.name)
 
 
         self.stdout.write(
