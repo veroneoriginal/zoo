8ab380600d6f4a70e494fffcb07cfb7ad815714c
veroneoriginal <veronikanizovaya@gmail.com>
2024-07-09 15:10:00 +0300

    add refactor models

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,21 +1,29 @@
 from django.db import models
 
 
-class Category(models.Model):
+class TimeStampMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(TimeStampMixin):
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return f'{self.name}'
 
 
-class Food(models.Model):
+class Food(TimeStampMixin):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return f'{self.name}'
 
 
-class Animal(models.Model):
+class Animal(TimeStampMixin):
     name = models.CharField(max_length=100)
     # 1 - 1, 1 - *, * - *
     # 1 категория - много животных
