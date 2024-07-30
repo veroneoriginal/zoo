5395232953f65c246b07bd9debb366d30929c731
veroneoriginal <veronikanizovaya@gmail.com>
2024-07-31 00:31:39 +0300

    make test for context and content

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -5,10 +5,7 @@ from mainapp.models import Category, Animal
 
 class TestCategory(TestCase):
 
-    def test_some(self):
-        self.assertEqual(1, 1)
-
-    def test_str(self):
+    def test_name_category_is_str(self):
         category = Category.objects.create(name="some name")
         self.assertEqual(str(category), "some name")
 
@@ -19,3 +16,32 @@ class TestAnimal(TestCase):
         category = Category.objects.create(name="some name")
         animal = Animal.objects.create(category=category, name="Roni")
         self.assertEqual(animal.get_category_name(), "some name")
+
+
+class TestCategorylistView(TestCase):
+
+    def test_status_code(self):
+        url = '/category/list/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_context(self):
+        url = '/category/list/'
+        response = self.client.get(url)
+        context = response.context
+        self.assertIn('object_list', context)
+        category_list = context['object_list']
+        self.assertEqual(len(category_list), 0)
+
+        category = Category.objects.create(name="some name")
+        response = self.client.get(url)
+        context = response.context
+        category_list = context['object_list']
+        self.assertEqual(len(category_list), 1)
+
+    def test_content(self):
+        category = Category.objects.create(name="some name")
+        url = '/category/list/'
+        response = self.client.get(url)
+        content = response.content
+        self.assertContains(response, "some name", 1)
