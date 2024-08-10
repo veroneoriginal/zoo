e8d6755b049752506f730d3de10394411d3cfefd
veroneoriginal <veronikanizovaya@gmail.com>
2024-08-10 19:58:54 +0300

    mixer is done

--- mainapp/management/commands/fake_mixer.py
+++ mainapp/management/commands/fake_mixer.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from mixer.backend.django import mixer
 from mainapp.models import (
     Category,
-    # Animal
+    Animal
 )
 
 
@@ -10,6 +10,7 @@ class Command(BaseCommand):
     help = "Fake data with mixer"
 
     Category.objects.all().delete()
+    Animal.objects.all().delete()
 
     # def handle(self, *args, **options):
     #
@@ -23,13 +24,36 @@ class Command(BaseCommand):
     #     print(animal.category.name)
 
     def handle(self, *args, **options):
-        # # использование mixer с lambda-функцией
-        # for i in range(5):
-        #     category = mixer.blend(Category, name=lambda: f'category{i}')
-        #     print(category.name)
+        """ Использование mixer с lambda-функцией """
+        # pylint: disable=W0640 cell-var-from-loop
+        for i in range(5):
+            mixer.blend(Category, name=lambda: f'lambda_func{i}')
 
-        my_list = ["Рыбка", "Свинка", "Крыска"]
-        # но можно и без нее (в этом случае)
+        # но можно и без lambda-функции (в этом случае)
+        my_list = ["Лошадь", "Собака", "Кошка"]
         for i in my_list:
-            category = mixer.blend(Category, name=f'{i}')
-            print(category)
+            mixer.blend(Category, name=f'{i}')
+
+        # генератор
+        mixer.cycle(4).blend(Category,
+                             name=(name for name in ('Piter', 'John', 'Leo', 'Tom')))
+
+        # Value could be getting a counter
+        categories = (mixer.cycle(4).
+                      blend(Category,
+                            name=mixer.sequence(lambda category: f"category_{category}")
+                            ))
+
+        for category in categories:
+            print('SEQUENCE', category.name, '\n')
+
+        client = mixer.blend(Animal, name=mixer.RANDOM)
+        print('RANDOM', client)
+
+        print(Category.objects.all().values_list('id'), '\n')
+
+        animals = mixer.blend(Animal)
+        print("without SELECT", animals.category.id, '\n')
+
+        animals = mixer.blend(Animal, category=mixer.SELECT)
+        print(animals.category.id)
