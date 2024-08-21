0fdd841bc99614934cfe823e0f06904d04e9993c
veroneoriginal <veronikanizovaya@gmail.com>
2024-08-21 19:54:34 +0300

    work before django-rq

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,20 +1,52 @@
+# В этом модуле код, который настроен для работы с Django Admin.
+# Django Admin — это встроенная панель администратора,
+# которая позволяет управлять моделями данных через веб-интерфейс.
+
+# admin - модуль, который предоставляет функциональность административной панели.
 from django.contrib import admin
 from .models import Animal, Category, Food, WildAnimal, HomeAnimal
 
+# регистрация модели Category в административной панели
 admin.site.register(Category)
-admin.site.register(Food)
 
 
+# ______________________________________________________________
+class FoodAdmin(admin.ModelAdmin):
+    """Класс настройки админки для еды"""
+
+    # list_display определяет, какие поля модели будут отображаться в админке
+    list_display = ('id', 'name')
+
+    @admin.action(description="Download Food")
+    def download_food(self, queryset):
+        with open('food.txt', 'w', encoding='utf-8') as f:
+            for food in queryset.all():
+                f.write(f'{food.name}\n')
+
+    # зарегать для отображениия в админке
+    actions = [download_food]
+
+
+# регистрация модели Food в административной панели
+admin.site.register(Food, FoodAdmin)
+
+
+# ______________________________________________________________
 class AnimalAdmin(admin.ModelAdmin):
+    """Класс настройки админки для отображения животных"""
+
     list_display = ('name', 'category', 'show_food')
 
 
+# регистрация модели животных и диких животных в административной панели
 admin.site.register(Animal, AnimalAdmin)
 admin.site.register(WildAnimal, AnimalAdmin)
 
 
 class HomeAnimalAdmin(admin.ModelAdmin):
+    """Класс настройки админки для отображения домашних животных"""
     list_display = ('name', 'category', 'last_owner_name')
 
 
+# регистрация модели домашних животных в административной панели
 admin.site.register(HomeAnimal, HomeAnimalAdmin)
--- mainapp/management/commands/create_admin.py
+++ mainapp/management/commands/create_admin.py
@@ -1,15 +1,16 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from django.db.utils import IntegrityError
+
+from user_app.models import MyUser
 
 
 class Command(BaseCommand):
     help = "Fill db with test data"
 
-    # pylint: disable=W0702 bare-except
     def handle(self, *args, **options):
         try:
-            User.objects.create_superuser('admin', 'admin@admin.com', 'qwerty')
-        except:
+            MyUser.objects.create_superuser('admin', 'admin@admin.com', 'qwerty')
+        except IntegrityError:
             self.stdout.write(self.style.SUCCESS('Already exists'))
         else:
             self.stdout.write(self.style.SUCCESS('Successfully created superuser'))
